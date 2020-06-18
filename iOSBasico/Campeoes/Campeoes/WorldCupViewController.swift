0636ea860ed3ed86e4e09883e40f45176463f24c
Douglas Frari <douglas_delfrari@apple.com>
2020-06-18 07:17:38 -0300

    Inicio do projeto da aula 3: Campeoes

---
+++ iOSBasico/Campeoes/Campeoes/WorldCupViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WorldCupViewController.swift
+//  Campeoes
+//
+//  Created by Douglas Frari on 17/06/20.
+//  Copyright © 2020 CESAR School. All rights reserved.
+//
+
+import UIKit
+
+class WorldCupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
