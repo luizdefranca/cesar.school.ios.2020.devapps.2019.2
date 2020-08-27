5e820ef8a17c317d24a4d830b1d92bf26db11ee8
Douglas Frari <douglas_delfrari@apple.com>
2020-08-26 21:44:40 -0300

    Aula 3

--- NetworkPersistencia/Semana1/MyGames/MyGames/GamesTableViewController.swift
+++ NetworkPersistencia/Semana1/MyGames/MyGames/GamesTableViewController.swift
@@ -12,6 +12,11 @@ import CoreData
 class GamesTableViewController: UITableViewController {
 
     
+    // tip. podemos passar qual view vai gerenciar a busca. Neste caso a própria viewController (logo usei nil)
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    
+    
     var label = UILabel()
     
     // esse tipo de classe oferece mais recursos para monitorar os dados
@@ -27,27 +32,50 @@ class GamesTableViewController: UITableViewController {
         label.text = "Você não tem jogos cadastrados"
         label.textAlignment = .center
         
+        // altera comportamento default que adicionava background escuro sobre a view principal
+         searchController.obscuresBackgroundDuringPresentation = false
+         searchController.searchBar.tintColor = .white
+         searchController.searchBar.barTintColor = .white
+        
+         navigationItem.searchController = searchController
+        
+         // usando extensions
+         searchController.searchBar.delegate = self
+         searchController.searchResultsUpdater = self
+        
+        
         loadGames()
     }
     
-    func loadGames() {
-        
-        // Coredata criou na classe model uma funcao para recuperar o fetch request
+    override func viewWillAppear(_ animated: Bool) {
+       super.viewWillAppear(animated)
+       // se ocorrer mudancas na entidade Console, a atualização automatica não irá ocorrer porque nosso NSFetchResultsController esta monitorando a entidade Game. Caso tiver mudanças na entidade Console precisamos atualizar a tela com a tabela de alguma forma: reloadData :)
+       tableView.reloadData()
+    }
+    
+    
+    // valor default evita precisar ser obrigado a passar o argumento quando chamado
+    func loadGames(filtering: String = "") {
         let fetchRequest: NSFetchRequest<Game> = Game.fetchRequest()
-               
-        // definindo criterio da ordenacao de como os dados serao entregues
-        let gameTitleSortDescriptor = NSSortDescriptor(key: "title", ascending: true)
-        fetchRequest.sortDescriptors = [gameTitleSortDescriptor]
-               
+        let sortDescriptor = NSSortDescriptor(key: "title", ascending: true)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+       
+        if !filtering.isEmpty {
+            // usando predicate: conjunto de regras para pesquisas
+            // contains [c] = search insensitive (nao considera letras identicas)
+            let predicate = NSPredicate(format: "title contains [c] %@", filtering)
+            fetchRequest.predicate = predicate
+        }
+       
+        // possui
         fetchedResultController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
         fetchedResultController.delegate = self
-               
+       
         do {
-             try fetchedResultController.performFetch()
-        } catch {
+            try fetchedResultController.performFetch()
+        } catch  {
             print(error.localizedDescription)
         }
-        
     }
 
     // MARK: - Table view data source
@@ -85,17 +113,26 @@ class GamesTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+                
         if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            
+            // TODO colocar um alerta de confirmacao aqui
+            
+            guard let game = fetchedResultController.fetchedObjects?[indexPath.row] else {return}
+            context.delete(game)
+            
+            do {
+                try context.save()
+            } catch  {
+                print(error.localizedDescription)
+            }
+        }
+        
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
@@ -112,15 +149,27 @@ class GamesTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        
+        //
+        if segue.identifier! == "gameSegue" {
+            print("gameSegue")
+            let vc = segue.destination as! GameViewController
+            
+            if let games = fetchedResultController.fetchedObjects {
+                vc.game = games[tableView.indexPathForSelectedRow!.row]
+            }
+            
+        }
+        
     }
-    */
+    
 
 } // fim da classe
 
@@ -140,5 +189,23 @@ extension GamesTableViewController: NSFetchedResultsControllerDelegate {
             default:
                 tableView.reloadData()
         }
+    }
+}
+
+
+extension GamesTableViewController: UISearchResultsUpdating, UISearchBarDelegate {
+    func updateSearchResults(for searchController: UISearchController) {
+        // TODO
+    }
+   
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        loadGames()
+        tableView.reloadData()
+       
+    }
+   
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        loadGames(filtering: searchBar.text!)
+        tableView.reloadData()
     }
 }
